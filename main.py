20c5ab45b889148c209ef2494f20deb7e9e651ea
Noneclue <noneclue713@gmail.com>
2019-06-26 02:29:45 +0900

    投球回/自責点のLabelを追加 root.afterする関数をupdate_money_after()として別に定義

--- main.py
+++ main.py
@@ -22,8 +22,27 @@ def update_money():
         third = 0
 
     income = str(500 * ((ini * 3) + third) - 5000 * lost)
-    money["text"] = income
-    root.after(300, update_money)
+    money["text"] = income + "万円"
+
+
+def update_money_after():
+    income = tk.StringVar()
+    ini = int(innings.get())
+    third = 0
+    lost = int(er.get())
+    get_one_third = one_third.get()
+
+    if get_one_third == "1/3":
+        third = 1
+    elif get_one_third == "2/3":
+        third = 2
+    else:
+        third = 0
+
+    income = str(500 * ((ini * 3) + third) - 5000 * lost)
+    money["text"] = income + "万円"
+
+    root.after(500, update_money_after)
 
 
 def refresh():
@@ -78,19 +97,23 @@ display.pack(fill=tk.X)
 mid = tk.Frame(root)
 
 update = tk.Button(mid, text="更新", command=refresh)
+inning_label = tk.Label(mid, text="投球回:")
 innings = tk.Spinbox(
     mid, from_=0, to=15, increment=1, width=4)
 ary = ["0/3", "1/3", "2/3"]
 one_third = tk.Spinbox(mid, value=ary, width=4, wrap=True)
+er_label = tk.Label(mid, text="失点:")
 er = tk.Spinbox(mid, from_=0, to=99, width=4)
 # earned run = 自責点（英語）
 money = tk.Label(mid, text="1試合の収支", relief="groove", width=16)
 fix_button = tk.Button(mid, text="<<確定>>", command=fix)
 
 update.pack(padx=5, side="left")
+inning_label.pack(padx=5, side=tk.LEFT)
 innings.pack(side="left")
 one_third.pack(side="left")
-er.pack(padx=10, side="left")
+er_label.pack(padx=5, side="left")
+er.pack(side="left")
 money.pack(side="left")
 fix_button.pack(padx=5, side="right")
 
@@ -122,6 +145,6 @@ calc.pack(fill=tk.BOTH)
 calc.config(yscrollcommand=y_bar.set)
 
 # 成績更新用　スピンボックスのイベントに入れるべきか？
-update_money()
+update_money_after()
 
 root.mainloop()
